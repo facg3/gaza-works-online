8ea54c1331f62a13ae2bf0afe78e93aeaf28d385
NoureldeanSaed <Noureldean.saed@gmail.com>
2018-01-15 11:13:30 +0200

    Created the first route in controllers, the rout / takes you to hello world renamed the index.js files, the main src/index.js is now src/server.js the controllers/index.js is now controllers/mainController.js RELATES TO Set up project #26 file structure part

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const routes = require('./controllers/mainController');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.set('port', process.env.PORT || 4001);
+app.use(routes);
 
 module.exports = app;
