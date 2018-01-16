3b5f820dcc8047508946b95f4c8e810ee8c73f34
sultanassi95 <mrsultanassi@gmail.com>
2018-01-16 15:29:43 +0200

    desigining homePage, stage 1, no merge required, still-working #99

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const controllers = require('./controllers/mainController');
+
+const app = express();
+
+app.set('port', process.env.PORT || 4001);
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(favicon(path.join(__dirname, '..', 'public', 'img', 'favicon.ico')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  layoutsDir: path.join(__dirname, 'views', 'layouts'),
+  partialsDir: path.join(__dirname, 'views', 'partials'),
+  defaultLayout: 'main',
+}));
+
+app.use(controllers);
+
+module.exports = app;
---
+++ src/controllers/homePage.js
@@ -0,0 +1,3 @@
+exports.get = (req, res) => {
+  res.render('homePage', { title: 'Gaza Works Online - Home', style: 'homePage' });
+};
