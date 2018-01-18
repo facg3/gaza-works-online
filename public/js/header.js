f147f9bdfe217531e8e193f86e3da4d04f63a3e1
NoureldeanSaed <Noureldean.saed@gmail.com>
2018-01-18 13:18:09 +0200

    Completed the navbar with its login form

--- public/js/header.js
+++ public/js/header.js
@@ -2,7 +2,6 @@ function toggleDropMenu() {
   document.getElementsByClassName('navbar')[0].classList.toggle('toggle-dm');
 }
 
-
 // This is a streatch goal feature, I will complete this later
 // Do not block the merge because of this
 // function toggleSearchBox() {
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const homePage = require('./homePage');
+const login = require('./login');
 
 const router = express.Router();
 
 router.get('/', homePage.get);
+router.post('/login', login.process);
 
 module.exports = router;
