b1d50642a4e9621777a38c9cb47e7b0cd0740466
noureldean <crow@localhost.localdomain>
2018-01-22 12:35:35 +0200

    still fixing conflicts

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,12 +1,8 @@
 const compareLogin = require('../database/queries/compareLogin');
-const jwt = require('jsonwebtoken');
-// const secret = require('env2')('config.env');
 
 exports.post = (req, res) => {
   compareLogin.compare(req.body, (err, compareResponse) => {
     if (err) throw new Error(err);
-    res.setHeader('Set-Cookie', ['laggeed=true', 'Max_Age=240*600*600']);
-    console.log('abdcds');
     res.send({ login_Status: compareResponse });
   });
 };
