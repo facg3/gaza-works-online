889bb0b9e2c2a2d5377a59933b6b0430db901f62
sultanassi95 <mrsultanassi@gmail.com>
2018-01-23 10:56:56 +0200

    divide and conquer, deleted irrelevant files. #74

--- src/controllers/viewProject.js
+++
@@ -1,7 +0,0 @@
-const { viewProject } = require('../database/queries/queries');
-
-exports.get = (req, res) => {
-  viewProject((err, result) => {
-    res.render('viewProject', { title: 'Project', style: 'singleCategory', projects: result });
-  });
-};
