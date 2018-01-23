687a4701bdfc19b076b765bd9740d8a1e50fafde
sultanassi95 <mrsultanassi@gmail.com>
2018-01-23 10:39:23 +0200

    Fixing Styles, and controller actions #74

--- src/controllers/singleCategory.js
+++ src/controllers/singleCategory.js
@@ -5,7 +5,7 @@ exports.get = (req, res) => {
   const reqCategory = req.params.singleCategory.replace(/-/g, ' ');
   selectCatProjects(reqCategory, (err, result) => {
     res.render('singleCategory', {
-      project: result, reqCategory, style: '../../css/singleCategory',
+      project: result, reqCategory, style: 'singleCategory',
     });
   });
 };
